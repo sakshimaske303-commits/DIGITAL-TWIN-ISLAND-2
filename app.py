65b76306b82ee72e5bb452fe113c2ebb70c19c87
sakshimaske303-commits <sakshimaske303@gmail.com>
2026-06-20 17:15:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(page_title="Digital Twin Island", layout="wide")
+
+st.title("🌍 Digital Twin of Climate-Vulnerable Island Systems")
+
+st.subheader("Can We Predict the Future of an Island Before It Disappears?")
+
+st.write("""
+This project develops a Digital Twin Framework for climate-vulnerable island systems.
+
+Study Areas:
+- Maldives
+- Lakshadweep
+- Seychelles
+- Fiji
+- Canary Islands
+
+The framework integrates climate, ecological, economic and social systems
+to assess future vulnerability and resilience under climate change scenarios.
+""")
